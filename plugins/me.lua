28e73f57fc1267348278c22a0a44ee122bcb73e3
bestroidd <bestroid78@gmail.com>
2016-06-17 20:57:11 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -6,10 +6,9 @@ local reply_id = msg['id']
 local info = '#Name : '..msg.from.first_name..'\n===================\n'
 ..'#Last name : '..(msg.from.last_name or '')..'\n===================\n'
 ..'#Id : '..msg.from.id..'\n===================\n'
-..'#Phone : +'..(msg.from.phone or '')..'\n===================\n'
 ..'#Username : telegram.me/'..(msg.from.username or '')..'\n===================\n'
 ..'#Group Id : '..msg.to.id..'\n===================\n'
-..'#Group name : '..msg.to.title..'\n===================\n@WaderTGTeam'
+..'#Group name : '..msg.to.title..'\n===================\n'
 
 reply_msg(reply_id, info, ok_cb, false)
 end
